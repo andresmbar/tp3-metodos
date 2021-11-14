78110ef31c3e48201f49521bcb3f6fb1400bec78
Oscar Alvarez <oscar_algu@hotmail.com>
2021-11-14 18:11:23 -0300

    saco metodos irrelevantes de lsq

--- src/lsq.h
+++ src/lsq.h
@@ -8,17 +8,8 @@ using namespace std;
 class LeastSquareMethod {
 public:
     LeastSquareMethod();
-
-    void fit(Matrix &A, Vector &b);
-    void retrieve_matrix_from_file(string file);
+  
     Vector ajustar(Matrix &A, Vector &b);
-
-private:
-
-    //Vector solveUpperTriangular(Matrix &A, Vector &b);
-    //pair<Matrix, Vector> GaussianElimination(Matrix &A, Vector &b);
-    std::vector<tuple<Eigen::VectorXd, int>> imagenes;
-
 
 
 };
